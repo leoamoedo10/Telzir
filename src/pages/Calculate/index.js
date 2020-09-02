676bb9bec4c0b2bc0b7daaaf6389ad70299b7a26
leoamoedo10 <leoamoedo10@gmail.com>
2020-09-02 17:22:27 -0300

    creation form page 'information'

--- src/pages/Calculate/index.js
+++ src/pages/Calculate/index.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import PageHeader from '../../components/PageHeader';
+import React from "react";
 
-import './styles.css';
+import PageHeader from "../../components/PageHeader";
 
-function Calculate() {
-  return(
-    <PageHeader />
-  );
+import "./styles.css";
+
+export default class Calculate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("Seu sabor favorito é: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <PageHeader />
+        </div>
+        <form onSubmit={this.handleSubmit} className="calcule">
+          <div className="select-block">
+            <select value="" required>
+              <option value="" disabled hidden>
+                Origem (DDD)
+              </option>
+              <option value="011">011</option>
+              <option value="016">016</option>
+              <option value="017">017</option>
+              <option value="018">018</option>
+            </select>
+
+            <select value="" required>
+              <option value="" disabled hidden>
+                Destino (DDD)
+              </option>
+              <option value="011">011</option>
+              <option value="016">016</option>
+              <option value="017">017</option>
+              <option value="018">018</option>
+            </select>
+
+            <div className="input-block">
+              <label>Minutos de ligação</label>
+              <input type="number" />
+            </div>
+
+            <select value="" required>
+              <option value="" disabled hidden>
+                Plano FaleMais
+              </option>
+              <option value="30">FaleMais 30</option>
+              <option value="60">FaleMais 60</option>
+              <option value="120">FaleMais 120</option>
+            </select>
+          </div>
+          <button type="submit">Calcular</button>
+        </form>
+      </>
+    );
+  }
 }
-
-export default Calculate;
