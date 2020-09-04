d096026c922f88be637b26a50b1601280cbc2476
leoamoedo10 <leoamoedo10@gmail.com>
2020-09-04 13:39:52 -0300

    create refs in inputs

--- src/pages/Calculate/index.js
+++ src/pages/Calculate/index.js
@@ -5,19 +5,21 @@ import PageHeader from "../../components/PageHeader";
 import "./styles.css";
 
 export default class Calculate extends React.Component {
+  submitForm = (e) => {
+    e.preventDefault();
 
-  submitForm = (event) => {  
-    event.preventDefault();
-
-    if (Number(this.inputOrigin.value) === this.inputDestiny.value) {
-      alert('O campo Origem não pode ser igual ao campo Destino!');
-    } if (Number(this.inputTime.value) < 0) {
-      alert('O campo Minutos não pode ser negativo. Tente novamente!');
+    if (this.inputOrigin.value === this.inputDestiny.value) {
+      alert("O campo Origem não pode ser igual ao campo Destino!");
+    } else if (Number(this.inputTime.value) < 0) {
+      alert("O campo Minutos não pode ser negativo. Tente novamente!");
     } else {
-      alert('Funcionou!');
+      alert("Funcionou!");
+      // let origin = Number(this.inputOrigin.value);
+      // let destiny = Number(this.inputDestiny.value);
+      // let plan = Number(this.inputPlan.value);
+      // let time = Number(this.inputTime.value);
     }
-
-  }
+  };
 
   render() {
     return (
@@ -25,44 +27,63 @@ export default class Calculate extends React.Component {
         <div>
           <PageHeader />
         </div>
-        <form onSubmit={this.submitForm} className="calcule">
-          <div className="select-block">
-            <select ref={(input) => this.inputOrigin = input} required>
-              <option value="" disabled hidden>
-                Origem (DDD)
-              </option>
-              <option value="011">011</option>
-              <option value="016">016</option>
-              <option value="017">017</option>
-              <option value="018">018</option>
-            </select>
 
-            <select ref={(input) => this.inputDestiny = input} required>
-              <option value="" disabled hidden>
-                Destino (DDD)
-              </option>
-              <option value="011">011</option>
-              <option value="016">016</option>
-              <option value="017">017</option>
-              <option value="018">018</option>
-            </select>
+        <div id="page-calculate">
+          <form onSubmit={(e) => this.submitForm(e)} className="calcule">
+            <div className="select-block">
+              <select
+                defaultValue=""
+                ref={(input) => (this.inputOrigin = input)}
+                required
+              >
+                <option value="" disabled hidden>
+                  Origem (DDD)
+                </option>
+                <option value="11">011</option>
+                <option value="16">016</option>
+                <option value="17">017</option>
+                <option value="18">018</option>
+              </select>
 
-            <div className="input-block">
-              <label>Minutos de ligação</label>
-              <input type="number" ref={(input) => {this.inputTime = input}} required/>
+              <select
+                defaultValue=""
+                ref={(input) => (this.inputDestiny = input)}
+                required
+              >
+                <option value="" disabled hidden>
+                  Destino (DDD)
+                </option>
+                <option value="11">011</option>
+                <option value="16">016</option>
+                <option value="17">017</option>
+                <option value="18">018</option>
+              </select>
+
+              <div className="input-block">
+                <label>Minutos de ligação</label>
+                <input
+                  type="number"
+                  ref={(input) => (this.inputTime = input)}
+                  required
+                />
+              </div>
+
+              <select
+                defaultValue=""
+                ref={(input) => (this.inputPlan = input)}
+                required
+              >
+                <option value="" disabled hidden>
+                  Plano FaleMais
+                </option>
+                <option value="30">FaleMais 30</option>
+                <option value="60">FaleMais 60</option>
+                <option value="120">FaleMais 120</option>
+              </select>
             </div>
-
-            <select ref={(input) => this.inputPlan = input} required>
-              <option value="" disabled hidden>
-                Plano FaleMais
-              </option>
-              <option value="30">FaleMais 30</option>
-              <option value="60">FaleMais 60</option>
-              <option value="120">FaleMais 120</option>
-            </select>
-          </div>
-          <button type="submit">Calcular</button>
-        </form>
+            <button type="submit">Calcular</button>
+          </form>
+        </div>
       </>
     );
   }
